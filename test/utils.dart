63cb37b00c13b85133df3797df399bc06c35b0dd
IvanLjubicicSW <ivan.ljubicic@stack-wizard.com>
2025-01-15 22:07:43 +0100

    test updates

--- test/utils.dart
+++ test/utils.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -45,7 +44,6 @@ Future prepareTests(WidgetTester tester) async {
     return;
   }
 
-  tester.binding.addTime(const Duration(seconds: 10));
   prepared = true;
   final fontData = File('test/assets/Roboto-Regular.ttf')
       .readAsBytes()
