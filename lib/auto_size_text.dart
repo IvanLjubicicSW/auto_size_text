8617176de4902e8840bff2b0edec2c39d03cc917
IvanLjubicicSW <ivan.ljubicic@stack-wizard.com>
2025-01-15 22:11:42 +0100

    forgot to exclude

--- lib/auto_size_text.dart
+++ lib/auto_size_text.dart
@@ -3,6 +3,5 @@
 library auto_size_text;
 
 export 'src/auto_size_builder/auto_size_builder.dart';
-export 'src/auto_size_text_field.dart';
 export 'src/auto_size_text.dart';
 export 'src/selectable_auto_size_text.dart';
